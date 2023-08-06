0ae739152e4510389da7c6f6c73261e4a81efa7f
Max Ossowski <ossowski.maximilian@gmail.com>
2023-08-06 16:18:04 +0200

    Add JS function with arguments

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ console.log(header)
 
 header.innerHTML = 'To jest tekst wygenerowany przez JS'
 
+function helloFunc(name, age) {
+    console.log("Your name is " + name)
+    console.log("You are " + age + " years old")
+}
+
+helloFunc("Max", 33)
