7d6735285b566d3af8e61280f531e3d5adc587d1
Max Ossowski <ossowski.maximilian@gmail.com>
2023-09-03 22:31:38 +0200

    Organize scrypts in seperate folder, add dat-fns and test script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+import differenceInDays from "date-fns/differenceInDays"
+
 const header = document.querySelector('.article__popup-text--js')
 
 header.innerHTML = 'To jest tekst wygenerowany przez JS'
@@ -9,3 +11,9 @@ function helloFunc(name, age) {
 
 helloFunc("Max", 33)
 
+const resultDays = differenceInDays(
+    new Date(2024, 4, 28, 0, 0),
+    new Date(2023, 8, 3, 0, 0)
+)
+
+console.log("Days too my birthday: " + resultDays)
