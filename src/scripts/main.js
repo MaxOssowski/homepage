545df8a13451d82c4fded3f40d63c3c54eec8da4
Max Ossowski <ossowski.maximilian@gmail.com>
2023-09-05 19:58:08 +0200

    Organize code in main.js and add function with array which counts sum of numbers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@ import differenceInDays from "date-fns/differenceInDays"
 import { substractNumbers } from "./calculate"
 
 const header = document.querySelector('.article__popup-text--js')
-
+const newArray = [2, 5, 16, 92, -25];
 header.innerHTML = 'To jest tekst wygenerowany przez JS'
 
 function helloFunc(name, age) {
@@ -10,13 +10,28 @@ function helloFunc(name, age) {
     console.log("You are " + age + " years old")
 }
 
-helloFunc("Max", 33)
+function countArray(myArray) {
+    sum = 0;
+    for(i = 0; i < myArray.length; i++) {
+        sum += myArray[i];
+    }
+    return sum;
+}
 
 const resultDays = differenceInDays(
     new Date(2024, 4, 28, 0, 0),
     new Date(2023, 8, 3, 0, 0)
 )
 
-console.log("Days too my birthday: " + resultDays)
+helloFunc("Max", 33)
 
+console.log("Days too my birthday: " + resultDays)
 console.log("9 - 3 = " + substractNumbers(9, 3));
+
+console.log("Array contains: ");
+for(i = 0; i < newArray.length; i++){
+    console.log(newArray[i])
+}
+
+console.log("Sum of elements in array: " + countArray(newArray));
+
